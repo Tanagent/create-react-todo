a00092a71ab8c21d72a8ee189f3eb79f5c72fe84
Brian Tan <briantanw@gmail.com>
2020-02-09 10:40:16 -0800

    second commit

---
+++ src/components/Todos.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
+
+class Todos extends Component {
+
+  render() {
+    
+    return this.props.todos.map((stuff) => (
+      <TodoItem key={stuff.id} stuff={stuff} 
+      markComplete={this.props.markComplete}  
+      delTodo={this.props.delTodo}/>
+    ));
+  }
+}
+
+// PropTypes
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
+}
+
+export default Todos;
